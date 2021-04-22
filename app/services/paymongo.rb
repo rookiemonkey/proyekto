d3e5b55b7d614c8ebb95aa9f0d36e4903bf372c6
rookiemonkey <kevinroirigorbasina@pm.me>
2021-04-22 13:11:09 +0800

    :heavy_plus_sign: add payment intent creation

--- app/services/paymongo.rb
+++ app/services/paymongo.rb
@@ -1,23 +1,48 @@
-# https://developers.paymongo.com/docs/accepting-cards4
-# payment_method_id is default for 'card'
 require 'json'
 require 'base64'
 
+# https://developers.paymongo.com/docs/accepting-cards4
+# payment_method_id is default for 'card'
+
 class Paymongo
-  @@base_url = 'https://api.paymongo.com/v1/payment_intents'.freeze
-  @@payment_method_id = 'pm_ajeDG2y6WgnrCXaamWFmPUw2'.freeze
-  @@paid_plans = Plans.available_plans
+  @base_url = 'https://api.paymongo.com/v1'.freeze
+  @payment_methods_url = "#{@base_url}/payment_methods"
+  @payment_intents_url = "#{@base_url}/payment_intents"
+  @headers = { 'Content-Type': 'application/json', Authorization: "Basic #{Base64.encode64(ENV['PAYMONGO_SK'])}" }
+  @paid_plans = Plans.available_plans
 
-  def self.new_payment_intent_for(plan)
-    raise 'Invalid given plan' unless @@paid_plans.key?(plan)
+  def self.new_payment_method(card_details)
+    parameters = JSON.generate(
+      {
+        data: {
+          attributes: {
+            type: 'card',
+            details: {
+              card_number: card_details[:card_number].to_s,
+              exp_month: card_details[:exp_month],
+              exp_year: card_details[:exp_year],
+              cvc: card_details[:cvc].to_s
+            }
+          }
+        }
+      }
+    )
 
-    return { PAYMENT_METHOD_ID: 'free' } if plan == :free
+    raw_response = Faraday.post(@payment_methods_url, parameters, @headers)
+    parsed_response = JSON.parse(raw_response.body)
+    parsed_response['data']['id']
+  end
+
+  def self.new_payment_intent(intent_details)
+    raise 'Invalid given plan' unless @paid_plans.key?(intent_details[:plan])
+
+    return { PAYMENT_METHOD_ID: 'free' } if intent_details[:plan] == :free
 
     parameters = JSON.generate(
       {
         data: {
           attributes: {
-            amount: @@paid_plans[plan],
+            amount: @paid_plans[intent_details[:plan]],
             payment_method_allowed: ['card'],
             payment_method_options: {
               card: { request_three_d_secure: 'any' }
@@ -28,16 +53,11 @@ class Paymongo
       }
     )
 
-    headers = {
-      'Content-Type': 'application/json',
-      Authorization: "Basic #{Base64.encode64(ENV['PAYMONGO_SK'])}"
-    }
-
-    raw_response = Faraday.post(@@base_url, parameters, headers)
+    raw_response = Faraday.post(@payment_intents_url, parameters, @headers)
     parsed_response = JSON.parse(raw_response.body)
     client_key = parsed_response['data']['attributes']['client_key']
     payment_intent_id = client_key.split('_client').first
 
-    { PAYMENT_METHOD_ID: @@payment_method_id, CLIENT_KEY: client_key, PAYMENT_INTENT_ID: payment_intent_id }
+    { PAYMENT_METHOD_ID: intent_details[:payment_method_id], CLIENT_KEY: client_key, PAYMENT_INTENT_ID: payment_intent_id }
   end
 end
--- spec/utilities/paymongo_spec.rb
+++ spec/utilities/paymongo_spec.rb
@@ -1,29 +1,48 @@
 require 'rails_helper'
 
+# https://developers.paymongo.com/docs/testing#basic-test-card-numbers
+
 RSpec.describe Paymongo do
-  describe 'Unsuccessful API Calls' do
-    it 'raises runtime error for invalid plans' do
-      expect { described_class.new_payment_intent_for(:not_a_plan) }.to raise_error(RuntimeError)
+  let(:card_details) do
+    {
+      card_number: 4_343_434_343_434_345,
+      exp_month: Time.zone.today.month + 1,
+      exp_year: Time.zone.today.year,
+      cvc: 258
+    }
+  end
+
+  let(:payment_method_id) { described_class.new_payment_method(card_details) }
+
+  describe 'Payment Methods' do
+    it 'return the payment method id' do
+      expect(described_class.new_payment_method(card_details).class).to eq(String)
     end
   end
 
-  describe 'Succesful API Calls (paid plans)' do
-    let(:response) { described_class.new_payment_intent_for(:standard) }
+  describe 'Payment Intents (Invalid Plans)' do
+    it 'raises runtime error for invalid plans' do
+      expect { described_class.new_payment_intent({ plan: :not_a_plan, payment_method_id: payment_method_id }) }.to raise_error(RuntimeError)
+    end
+  end
+
+  describe 'Payment Intents (Paid Plans)' do
+    let(:data) { described_class.new_payment_intent({ plan: :enterprise, payment_method_id: payment_method_id }) }
 
     it 'returns a hash with 3 keys' do
-      expect(response.keys.length).to eq(3)
+      expect(data.keys.length).to eq(3)
     end
 
     it 'has PAYMENT_METHOD_ID key' do
-      expect(response.keys.include?(:PAYMENT_METHOD_ID)).to eq(true)
+      expect(data.keys.include?(:PAYMENT_METHOD_ID)).to eq(true)
     end
 
     it 'has CLIENT_KEY key' do
-      expect(response.keys.include?(:CLIENT_KEY)).to eq(true)
+      expect(data.keys.include?(:CLIENT_KEY)).to eq(true)
     end
 
     it 'has PAYMENT_INTENT_ID key' do
-      expect(response.keys.include?(:PAYMENT_INTENT_ID)).to eq(true)
+      expect(data.keys.include?(:PAYMENT_INTENT_ID)).to eq(true)
     end
   end
 end
