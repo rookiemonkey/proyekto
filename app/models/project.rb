f34e6270043a254b110608429f6ce62e53229a34
rookiemonkey <kevinroirigorbasina@pm.me>
2021-04-26 09:10:48 +0800

    :heavy_plus_sign: add project to plan validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,27 @@ class Project < ApplicationRecord
   acts_as_tenant(:organization)
   has_many :artifacts, dependent: :destroy
 
+  validate :organization_plan_restrictions
   validates :name, presence: true
+
+  private
+
+  def organization_plan_restrictions
+    return unless organization
+
+    restrict_by_plan if plan_is_free_and_reaches_project_limit
+    restrict_by_plan if plan_is_standard_and_reaches_project_limit
+  end
+
+  def restrict_by_plan
+    errors.add(:organization, 'plan restriction')
+  end
+
+  def plan_is_free_and_reaches_project_limit
+    organization.plan == 'free' && organization.projects.count == Plans.available_plans[:free][:project_limit]
+  end
+
+  def plan_is_standard_and_reaches_project_limit
+    organization.plan == 'standard' && organization.projects.count == Plans.available_plans[:standard][:project_limit]
+  end
 end
