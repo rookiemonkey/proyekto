0d21389a7df728dcbabdbbf9c20fd1f5dd6bad4f
rookiemonkey <kevinroirigorbasina@pm.me>
2021-05-17 21:01:33 +0800

    :heavy_plus_sign: add toast notifications

---
+++ app/javascript/packs/toast.js
@@ -0,0 +1,22 @@
+window.HTMLElementToaster = class HTMLElementToaster {
+  initialize() {
+    this.hideTimeout = null;
+    this.element = document.createElement("div");
+    this.element.className = "toast";
+    document.body.appendChild(this.element);
+  }
+
+  showMessage = (message, state) => {
+    clearTimeout(this.hideTimeout);
+    this.element.textContent = message;
+    this.element.className = "toast toast--visible";
+
+    if (state) {
+      this.element.classList.add(`toast--${state}`);
+    }
+
+    this.hideTimeout = setTimeout(() => {
+      this.element.classList.remove("toast--visible");
+    }, 3000);
+  }
+}
