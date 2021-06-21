2c5369234af3df5f34e46083f0f8a3c774448f6f
rookiemonkey <kevinroirigorbasina@pm.me>
2021-06-21 17:33:15 +0800

    :heavy_plus_sign: add activities on colleagues view

--- app/controllers/organization_controller.rb
+++ app/controllers/organization_controller.rb
@@ -9,6 +9,7 @@ class OrganizationController < ApplicationController
 
   def colleagues
     @colleagues = User.all
+    @activities = Activity.where(activity_type: 'staff').limit(10)
   end
 
   def artifacts
@@ -18,7 +19,7 @@ class OrganizationController < ApplicationController
   def update_plan
     success = current_tenant.update(new_plan)
     message = success ? 'Successfully updated your plan' : current_tenant.errors.full_messages.first
-    trigger_activity("#{current_user} updated your plan to '#{new_plan}'")
+    trigger_activity("#{current_user.full_name} updated your plan to '#{new_plan[:plan]}'")
     render json: { success: success, message: message }
   end
 
