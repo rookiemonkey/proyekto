d769d08f9fd83273d619525c33b1639190936636
rookiemonkey <kevinroirigorbasina@pm.me>
2021-07-09 08:41:09 +0800

    :wrench: replace dicebear avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def avatar_for(name)
-    url = "https://avatars.dicebear.com/v2/initials/#{name.parameterize}.svg"
-    image_tag url, alt: name, class: 'is-rounded'
+    image_tag "https://ui-avatars.com/api/?name=#{name}&background=4a4a4a&color=ffffff", alt: name, class: 'is-rounded'
   end
 
   def date_in_full_words(date_object)
